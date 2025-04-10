51b8023a226c66f5f9871a101f038b203cad2e68
gmunareto <gerisson.munareto@grupostudio.com.br>
2025-04-10 18:27:26 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import SimuladorPejotizacao from './SimuladorPejotizacao'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
